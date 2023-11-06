0fe4a654b97806f2f8ac03f2ae7e2871626ae441
arnoud <arnoud@confucius.localdomain>
2023-11-06 22:45:52 +0100

    Initial commit

---
+++ src/main/Start.java
@@ -0,0 +1,16 @@
+package main;
+
+/**
+ *
+ * @author arnoud
+ */
+public class Start {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
