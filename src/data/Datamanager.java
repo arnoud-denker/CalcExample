39be20eb38351eacbe7af793d8595fbc5159dae5
arnoud <arnoud@confucius.localdomain>
2023-11-06 23:03:43 +0100

    Create data

---
+++ src/data/Datamanager.java
@@ -0,0 +1,72 @@
+package data;
+
+import java.util.ArrayList;
+
+
+public class Datamanager 
+    {
+    public Datamanager()
+        {
+        
+        }
+    
+    public ArrayList <TimeValue> get_before_array()
+        {
+        ArrayList <TimeValue> before_list = new ArrayList();    
+            
+        before_list.add(new TimeValue(2459115.5,540.0));
+        before_list.add(new TimeValue(2459116.5,542.3));
+        before_list.add(new TimeValue(2459117.5,544.9));
+        before_list.add(new TimeValue(2459120.5,552.1));
+        before_list.add(new TimeValue(2459121.5,548.8));
+        before_list.add(new TimeValue(2459122.5,547.6));
+        before_list.add(new TimeValue(2459123.5,551.6));
+        before_list.add(new TimeValue(2459124.5,553.9));
+        before_list.add(new TimeValue(2459127.5,560.1));
+        before_list.add(new TimeValue(2459128.5,550.35));
+        before_list.add(new TimeValue(2459129.5,555.4));
+        before_list.add(new TimeValue(2459130.5,561.5));
+        before_list.add(new TimeValue(2459131.5,568.45));
+        before_list.add(new TimeValue(2459134.5,573.15));
+        before_list.add(new TimeValue(2459135.5,572.0));
+        before_list.add(new TimeValue(2459136.5,569.6));
+        before_list.add(new TimeValue(2459137.5,562.9));
+        before_list.add(new TimeValue(2459138.5,568.0));
+        before_list.add(new TimeValue(2459142.5,561.95));
+        before_list.add(new TimeValue(2459143.5,554.55));
+        before_list.add(new TimeValue(2459144.5,553.45));
+        
+        return before_list;
+        }
+    
+
+    public ArrayList <TimeValue> get_after_array()
+        {
+        ArrayList <TimeValue> after_list = new ArrayList();   
+            
+        after_list.add(new TimeValue(2459145.5,553.65));
+        after_list.add(new TimeValue(2459148.5,544.6));
+        after_list.add(new TimeValue(2459149.5,540.9));
+        after_list.add(new TimeValue(2459150.5,529.45));
+        after_list.add(new TimeValue(2459151.5,533.35));
+        after_list.add(new TimeValue(2459152.5,535.05));
+        after_list.add(new TimeValue(2459155.5,543.05));
+        after_list.add(new TimeValue(2459156.5,553.7));
+        after_list.add(new TimeValue(2459157.5,562.45));
+        after_list.add(new TimeValue(2459158.5,570.5));
+        after_list.add(new TimeValue(2459159.5,572.35));
+        after_list.add(new TimeValue(2459162.5,588.75));
+        after_list.add(new TimeValue(2459163.5,590.9));
+        after_list.add(new TimeValue(2459164.5,598.8));
+        after_list.add(new TimeValue(2459165.5,594.9));
+        after_list.add(new TimeValue(2459166.5,600.65));
+        after_list.add(new TimeValue(2459169.5,601.55));
+        after_list.add(new TimeValue(2459170.5,601.5));
+        after_list.add(new TimeValue(2459171.5,596.6));
+        after_list.add(new TimeValue(2459172.5,597.25));
+        after_list.add(new TimeValue(2459173.5,602.35));  
+        
+        return after_list;
+        }
+    
+    }
---
+++ src/data/TimeValue.java
@@ -0,0 +1,21 @@
+
+package data;
+
+/**
+ *
+ * @author arnoud
+ */
+public class TimeValue 
+    {
+    public final double date;
+    public final double value;
+
+    
+    public TimeValue(double _date, double _value)
+        {
+        date = _date;
+        value = _value;
+        }
+    
+    
+    }
